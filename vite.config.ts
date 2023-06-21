18188c5dc871225e85ea70e81214e81fc7562bff
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2023-06-21 10:42:37 +0800

    消灭打包产出文件名带的hash值

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,14 @@ export default defineConfig({
     }
   },
   build: {
-    assetsDir: "static/"
+    assetsDir: "static/",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   },
   base: "./",
   server: {
