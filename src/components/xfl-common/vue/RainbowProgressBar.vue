02b222dc67a44367e8423ee05a8704051a48e13d
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2023-06-23 14:38:58 +0800

    可以根据百分比动态计算颜色的进度条组件

---
+++ src/components/xfl-common/vue/RainbowProgressBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <progress-bar
+    :the-font-size-in-pixel="theFontSizeInPixel"
+    :the-percent="thePercent"
+    :the-border-color="borderColor"
+    :the-content-color="theContentColor"
+    :show-percent-number="showPercentNumber"
+    :props-percent-number-style="propsPercentNumberStyle"
+    :percent-number2-fixed="percentNumber2Fixed"
+  />
+</template>
+
+<script setup lang="tsx">
+import { computed, PropType, ref } from "vue";
+import tinygradient from "tinygradient";
+import ProgressBar from "./ProgressBar.vue";
+import { PartialCssStyleType } from "../ts/PartialCssStyleType";
+
+const props = defineProps({
+  theFontSizeInPixel: {
+    type: Number,
+    default: 24
+  },
+  thePercent: {
+    type: Number,
+    default: 0.8
+  },
+  theBorderColor: {
+    type: String,
+    default: "auto"
+  },
+  progressBarColorArray: {
+    type: Array as PropType<String[]>,
+    default: () =>
+      tinygradient("red", "aqua")
+        .hsv(100, "long")
+        .map((item) => item.toHexString())
+  },
+  showPercentNumber: {
+    type: Boolean,
+    default: false
+  },
+  propsPercentNumberStyle: {
+    type: Object as PropType<PartialCssStyleType>,
+    default: (): PartialCssStyleType => {
+      return {};
+    }
+  },
+  percentNumber2Fixed: {
+    type: Number,
+    default: 0
+  }
+});
+
+const theContentColor = computed(() => {
+  const arr = props.progressBarColorArray;
+  return arr[Math.round((arr.length - 1) * props.thePercent)];
+});
+
+const borderColor = computed(() => {
+  return props.theBorderColor === "auto" ? theContentColor.value : props.theBorderColor;
+});
+
+const templateRoot = ref<HTMLDivElement>();
+</script>
