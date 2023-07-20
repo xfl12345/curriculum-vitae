2999d5c0b6f35c36f48cad8ee701fb258f287278
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2023-07-20 11:12:56 +0800

    增加对常用引用根路径的别名的支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from "node:url";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -23,6 +25,7 @@ export default defineConfig({
   assetsInclude: ["**/*.bmp"],
   resolve: {
     alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
       "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js"
     }
   },
