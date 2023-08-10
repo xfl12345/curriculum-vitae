f20b311d1d113912ee8810db5c501e1a8eca5aae
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2023-08-10 11:00:48 +0800

    优化，新增支持穿透样式

--- src/components/xfl-common/vue/TextPrettier.vue
+++ src/components/xfl-common/vue/TextPrettier.vue
@@ -1,6 +1,8 @@
 <script lang="tsx">
-import { defineComponent, getCurrentInstance, h, VNode } from "vue";
+import type { VNodeProps, PropType, VNode } from "vue";
+import { defineComponent, h } from "vue";
 import isChinese from "is-chinese";
+import { PartialCssStyleType } from "@/components/xfl-common/ts/PartialCssStyleType";
 
 export default defineComponent({
   // name: "TextPrettier",
@@ -8,71 +10,35 @@ export default defineComponent({
     content: {
       type: String,
       default: ""
+    },
+    propsCssStyle: {
+      type: Object as PropType<PartialCssStyleType>,
+      default: (): PartialCssStyleType => {
+        return {
+          fontSize: "inherit"
+        };
+      }
     }
   },
-  setup(props, ctx) {
-    const isChineseCharacter = (character: string): Boolean => {
-      // return characterUnicode >= 0x4e00 && characterUnicode <= 0x9fa5;
-      return isChinese(character);
-    };
-
-    const generateChildrenVnode = () => {
-      const myself = getCurrentInstance();
-      // const vueData = myself!.data;
-
-      const theInputValue = props.content;
-      const itemCount = theInputValue!.length;
-      const theChildrenVnode: VNode[] = [];
-
-      let isPreviousCharacterChinese: Boolean = false;
-      let currentCharacter;
-      let currentPhase: string[] = [];
-
-      const pushNewSpanElement = () => {
-        const finalInnerText = currentPhase.join("");
-        const spanTagVnode = isPreviousCharacterChinese ? (
-          <span>{finalInnerText}</span>
-        ) : (
-          <span style={{ fontFamily: "Consolas, serif" }}>{finalInnerText}</span>
-        );
-
-        theChildrenVnode.push(spanTagVnode);
-      };
-
-      if (itemCount > 0) {
-        currentCharacter = theInputValue!.charAt(0);
-        isPreviousCharacterChinese = isChineseCharacter(currentCharacter);
-
-        currentPhase.push(currentCharacter);
-
-        // 如果相邻字符是同个字体分类，那么便挤在一起，不分家（大家都在同一个 span 元素里）
-        for (let i = 1; i < itemCount; i += 1) {
-          currentCharacter = theInputValue!.charAt(i);
-          const isCurrentCharacterChinese = isChineseCharacter(currentCharacter);
-
-          if (isPreviousCharacterChinese !== isCurrentCharacterChinese) {
-            pushNewSpanElement();
-
-            currentPhase = [];
-          }
-
-          isPreviousCharacterChinese = isCurrentCharacterChinese;
-
-          currentPhase.push(currentCharacter);
-        }
-
-        pushNewSpanElement();
-      }
-
-      return theChildrenVnode;
-    };
-
-    return { isChineseCharacter, generateChildrenVnode };
-  },
+  setup(props, ctx) {},
   data() {
     return {
       myValueBox: h("div")
     };
+  },
+  computed: {
+    theChineseContentStyle() {
+      return this.propsCssStyle;
+    },
+    theDefaultContentStyle() {
+      const myself = this;
+      const theStyle = { ...myself.propsCssStyle };
+      if (typeof myself.propsCssStyle.fontFamily === "undefined") {
+        theStyle.fontFamily = "Consolas, serif";
+      }
+
+      return theStyle;
+    }
   },
   watch: {
     content(newValue, oldValue) {
@@ -91,8 +57,79 @@ export default defineComponent({
     // }
   },
   methods: {
+    isChineseCharacter(character: string) {
+      // return characterUnicode >= 0x4e00 && characterUnicode <= 0x9fa5;
+      return isChinese(character);
+    },
+    initVnodeProps(vnode: VNode) {
+      if (typeof vnode.props === "undefined" || vnode.props === null) {
+        vnode.props = {} as VNodeProps;
+      }
+    },
     recreateTheValueBoxVnode() {
+      // const rootNode = <div style={{ display: "inline-block" }} />;
+      const rootNode = h(
+        "div",
+        {
+          style: { display: "inline-block" }
+        },
+        this.generateChildrenVnode()
+      );
+
       return h("div", this.generateChildrenVnode());
+    },
+    generateChildrenVnode() {
+      const myself = this;
+      // const vueData = myself!.data;
+
+      const theInputValue = myself.content;
+      const itemCount = theInputValue!.length;
+      const theChildrenVnode: VNode[] = [];
+
+      let isPreviousCharacterChinese: Boolean = false;
+      let currentCharacter;
+      let currentPhase: string[] = [];
+
+      const pushNewSpanElement = () => {
+        const finalInnerText = currentPhase.join("");
+        const spanTagVnode = <span>{finalInnerText}</span>;
+        if (spanTagVnode.props === null) {
+          spanTagVnode.props = {} as VNodeProps;
+        }
+
+        // 因为检测到字体语种的不同，所以触发了样式切换。
+        // 等待被压入节点群的节点，已经是过去式了。如果这个节点是中文的，将使用中文样式。
+        spanTagVnode.props.style = isPreviousCharacterChinese
+          ? myself.theChineseContentStyle
+          : myself.theDefaultContentStyle;
+
+        theChildrenVnode.push(spanTagVnode);
+      };
+
+      if (itemCount > 0) {
+        currentCharacter = theInputValue!.charAt(0);
+        isPreviousCharacterChinese = myself.isChineseCharacter(currentCharacter);
+
+        currentPhase.push(currentCharacter);
+
+        // 如果相邻字符是同个字体分类，那么便挤在一起，不分家（大家都在同一个 span 元素里）
+        for (let i = 1; i < itemCount; i += 1) {
+          currentCharacter = theInputValue!.charAt(i);
+          const isCurrentCharacterChinese = myself.isChineseCharacter(currentCharacter);
+
+          if (isPreviousCharacterChinese !== isCurrentCharacterChinese) {
+            pushNewSpanElement();
+            currentPhase = [];
+          }
+
+          isPreviousCharacterChinese = isCurrentCharacterChinese;
+          currentPhase.push(currentCharacter);
+        }
+
+        pushNewSpanElement();
+      }
+
+      return theChildrenVnode;
     }
   },
   render() {
